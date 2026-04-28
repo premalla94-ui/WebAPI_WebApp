45c5f309a5c473ad58b6c6ece937e5ebdd12535c
premalla <premchand122@outlook.com>
2026-04-28 15:42:27 +0530

    added unit test cases

---
+++ WebAPI_WebApp.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using WebAPI_WebApp.Controllers;
+
+namespace WebAPI_WebApp.Tests;
+
+public class WeatherForecastControllerTests
+{
+    [Fact]
+    public void Get_ReturnsFiveForecasts()
+    {
+        var controller = new WeatherForecastController(NullLogger<WeatherForecastController>.Instance);
+
+        var result = controller.Get().ToArray();
+
+        Assert.Equal(5, result.Length);
+        Assert.All(result, x => Assert.InRange(x.TemperatureC, -20, 55));
+    }
+}
